bcdc74dd3f8cde104e410a6b505421a793856a8e
Jose Copeti <jrcopeti@gmail.com>
2024-06-27 15:11:53 +0200

    iteration 1 complete

--- src/index.js
+++ src/index.js
@@ -2,25 +2,22 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
-
-
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
 
+const button = document.querySelector("#start-btn");
 
-
+button.addEventListener("click", () => {
+  console.log("button is clicked")
+})
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
 
-
   // Your code goes here ...
 }
-
-
-
 
 // ITERATION 3: Show Toast
 function showToast(message) {
@@ -28,11 +25,7 @@ function showToast(message) {
 
   // Your code goes here ...
 
-
-
-
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
-
 }
